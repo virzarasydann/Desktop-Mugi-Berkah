d230ecc4ff6d519c4a9e1d617015c150b478561a
virzarasydann <virzarasydan@gmail.com>
2026-04-17 23:51:28 +0700

    feat: move business logic from view to service
    feat: add transaksi controller

---
+++ TugasBesar/Controllers/TransaksiControllers.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using TugasBesar.Models;
+using TugasBesar.Services;
+
+namespace TugasBesar.Controllers
+{
+    public class TransaksiControllers
+    {
+        private TransaksiServices _services;
+
+        public TransaksiControllers()
+        {
+            _services = new TransaksiServices();
+        }
+
+        public List<ProdukModels> AmbilKatalog()
+        {
+            return _services.DapatkanKatalog();
+        }
+
+        public List<KeranjangItem> ProsesTambahKeranjang(string nama, int harga)
+        {
+            return _services.TambahProdukKeKeranjang(nama, harga);
+        }
+
+        public int AmbilGrandTotal()
+        {
+            return _services.HitungGrandTotal();
+        }
+
+        public int ProsesHitungKembalian(int uangDiterima)
+        {
+            return _services.HitungKembalian(uangDiterima);
+        }
+    }
+}
