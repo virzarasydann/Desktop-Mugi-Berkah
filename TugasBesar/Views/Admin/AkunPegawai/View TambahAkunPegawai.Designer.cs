9f8bbb433a10eea9ce7779a485b60fe06baa779a
Ridho Ramadhan <ridho.ramadan1016@gmail.com>
2026-05-07 21:09:59 +0700

    Menyelesaikan fitur CRUD, UI, dan hide password Akun Pegawai

--- TugasBesar/Views/Admin/AkunPegawai/View TambahAkunPegawai.Designer.cs
+++ TugasBesar/Views/Admin/AkunPegawai/View TambahAkunPegawai.Designer.cs
@@ -35,16 +35,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnTambahAkunPegawai = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvAkunPegawai = new System.Windows.Forms.DataGridView();
             this.btnSetTex = new System.Windows.Forms.Button();
             this.btnHapusAkunPegawai = new System.Windows.Forms.Button();
             this.btnEditAkunPegawai = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAkunPegawai)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,7 +54,7 @@
             this.panel1.Controls.Add(this.btnTambahAkunPegawai);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(470, 692);
             this.panel1.TabIndex = 0;
@@ -65,7 +62,7 @@
             // tbPassword
             // 
             this.tbPassword.Location = new System.Drawing.Point(71, 304);
-            this.tbPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(2);
             this.tbPassword.Multiline = true;
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.Size = new System.Drawing.Size(200, 36);
@@ -74,11 +71,12 @@
             // tbUsername
             // 
             this.tbUsername.Location = new System.Drawing.Point(71, 206);
-            this.tbUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(2);
             this.tbUsername.Multiline = true;
             this.tbUsername.Name = "tbUsername";
             this.tbUsername.Size = new System.Drawing.Size(200, 36);
             this.tbUsername.TabIndex = 3;
+            this.tbUsername.TextChanged += new System.EventHandler(this.tbUsername_TextChanged);
             // 
             // label2
             // 
@@ -106,31 +104,42 @@
             // 
             this.btnTambahAkunPegawai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTambahAkunPegawai.Location = new System.Drawing.Point(171, 431);
-            this.btnTambahAkunPegawai.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTambahAkunPegawai.Margin = new System.Windows.Forms.Padding(2);
             this.btnTambahAkunPegawai.Name = "btnTambahAkunPegawai";
             this.btnTambahAkunPegawai.Size = new System.Drawing.Size(153, 46);
             this.btnTambahAkunPegawai.TabIndex = 0;
             this.btnTambahAkunPegawai.Text = "Tambah Akun Pegawai";
             this.btnTambahAkunPegawai.UseVisualStyleBackColor = true;
+            this.btnTambahAkunPegawai.Click += new System.EventHandler(this.btnTambahAkunPegawai_Click);
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.dgvAkunPegawai);
             this.panel2.Controls.Add(this.btnSetTex);
             this.panel2.Controls.Add(this.btnHapusAkunPegawai);
             this.panel2.Controls.Add(this.btnEditAkunPegawai);
-            this.panel2.Controls.Add(this.tableLayoutPanel1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel2.Location = new System.Drawing.Point(468, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(655, 692);
             this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // dgvAkunPegawai
+            // 
+            this.dgvAkunPegawai.AccessibleName = "dgvAkunPegawai";
+            this.dgvAkunPegawai.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAkunPegawai.Location = new System.Drawing.Point(0, 286);
+            this.dgvAkunPegawai.Name = "dgvAkunPegawai";
+            this.dgvAkunPegawai.Size = new System.Drawing.Size(652, 244);
+            this.dgvAkunPegawai.TabIndex = 4;
             // 
             // btnSetTex
             // 
             this.btnSetTex.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSetTex.Location = new System.Drawing.Point(533, 234);
-            this.btnSetTex.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSetTex.Margin = new System.Windows.Forms.Padding(2);
             this.btnSetTex.Name = "btnSetTex";
             this.btnSetTex.Size = new System.Drawing.Size(74, 32);
             this.btnSetTex.TabIndex = 3;
@@ -141,7 +150,7 @@
             // 
             this.btnHapusAkunPegawai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnHapusAkunPegawai.Location = new System.Drawing.Point(346, 172);
-            this.btnHapusAkunPegawai.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnHapusAkunPegawai.Margin = new System.Windows.Forms.Padding(2);
             this.btnHapusAkunPegawai.Name = "btnHapusAkunPegawai";
             this.btnHapusAkunPegawai.Size = new System.Drawing.Size(164, 47);
             this.btnHapusAkunPegawai.TabIndex = 2;
@@ -152,63 +161,12 @@
             // 
             this.btnEditAkunPegawai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEditAkunPegawai.Location = new System.Drawing.Point(203, 172);
-            this.btnEditAkunPegawai.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEditAkunPegawai.Margin = new System.Windows.Forms.Padding(2);
             this.btnEditAkunPegawai.Name = "btnEditAkunPegawai";
             this.btnEditAkunPegawai.Size = new System.Drawing.Size(139, 47);
             this.btnEditAkunPegawai.TabIndex = 1;
             this.btnEditAkunPegawai.Text = "Edit Akun Pegawai";
             this.btnEditAkunPegawai.UseVisualStyleBackColor = true;
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.51872F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80.48129F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 282F));
-            this.tableLayoutPanel1.Controls.Add(this.label5, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(7, 286);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.10363F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.89637F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(627, 379);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(346, 0);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 17);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Password";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(69, 0);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(146, 17);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Username Pegawai";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(2, 0);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 17);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "No";
             // 
             // ViewTambahAkunPegawai
             // 
@@ -216,14 +174,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ViewTambahAkunPegawai";
             this.Size = new System.Drawing.Size(1123, 692);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAkunPegawai)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -237,12 +194,9 @@
         private System.Windows.Forms.TextBox tbPassword;
         private System.Windows.Forms.TextBox tbUsername;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvAkunPegawai;
         private System.Windows.Forms.Button btnSetTex;
         private System.Windows.Forms.Button btnHapusAkunPegawai;
         private System.Windows.Forms.Button btnEditAkunPegawai;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
     }
 }
