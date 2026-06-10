3aaf657dc168f603162a5c467d2c358e87491cbd
arkalevi <arkalevi2@gmail.com>
2026-06-11 01:49:38 +0700

    clean code: pindah validasi ke DTO menggunakan Data Annotations dan hapus kode berulang

--- src/TugasBesar.Core/DTO/Request/KategoriRequestDTO.cs
+++ src/TugasBesar.Core/DTO/Request/KategoriRequestDTO.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TugasBesar.Core.DTO.Request
 {
     public class KategoriRequestDTO
     {
+        [Required(ErrorMessage = "Nama kategori tidak boleh kosong!")]
         public string Nama { get; set; }
     }
 }
